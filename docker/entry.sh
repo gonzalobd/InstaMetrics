a5dbf8cebd67837985131ed527b7307674c0abe9
gonzalobd <gonzalobd@outlook.com>
2017-05-04 13:15:25 +0200

    editing dockerfile

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 cd $SPARK_HOME
-bin/spark-submit --jars lib/spark-streaming-kafka-assembly_2.10-1.6.2.jar /InstaMetrics/Main.py
+bin/spark-submit --jars lib/spark-streaming-kafka-assembly_2.10-1.6.2.jar /InstaMetrics/Main.py $BROKER_KAFKA $CASSANDRA
